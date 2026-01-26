567770581036ccfef639fee12ea7892d93ddf7e2
alopopau-eng <alopopau@gmail.com>
2026-01-26 19:49:41 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,14 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyB5wRVuMhuX32v4g8P4rDkB_NvlrKVUhno",
+  authDomain: "sdfg-98b06.firebaseapp.com",
+  projectId: "sdfg-98b06",
+  storageBucket: "sdfg-98b06.firebasestorage.app",
+  messagingSenderId: "307140724566",
+  appId: "1:307140724566:web:669d76007c14aeea33b8f7",
+  measurementId: "G-0J30YY52NS"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
